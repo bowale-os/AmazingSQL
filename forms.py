29b0e5047634a1d2c38d9b92d280b62f9e155988
Daniel Olasubomi Sobowale <danbowale@gmail.com>
2025-07-22 11:10:10 -0500

    Update forms.py
    Added feedback form

--- forms.py
+++ forms.py
@@ -27,19 +27,8 @@ class BioForm(FlaskForm):
     bio = TextAreaField("Your Bio: ", validators=[DataRequired(message="Bio cannot be empty"), Length(max=200, message="bio is too long")])
     submit = SubmitField("Add bio")
 
+class FeedbackForm(FlaskForm):
+    like = TextAreaField("What did you enjoy?")
+    dislike = TextAreaField("What did you dislike?")
+    hate = TextAreaField("What confused or frustrated you?")
 
-# class EditProfileForm(FlaskForm):
-#     # display_name = StringField("Display Name", validators=
-#     #     [DataRequired(), Length(max=60, message="Name is too long")
-#     # ])
-
-#     bio = TextAreaField("Bio", validators=[
-#         Length(max=200, message="Bio is too long")
-#     ])
-
-#     profile_pic = FileField("Profile picture", validators=[
-#         FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
-#     ])
-#     # email = StringField("Email: ", validators=[DataRequired(), Length(max=40, message="Email is too long")])
-#     # password = PasswordField('Password', validators=[DataRequired(), Length(min=10, message="Password should be 10 characters or longer")])
-#     submit = SubmitField('Save Changes')
